56e6728f39013b185140925e8413a7250142ac67
Marco Pontiz <marcopontiz@gmail.com>
2023-05-11 16:17:09 -0300

    Añadiendo nuevas carpetas al proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Usuario = require('../models/usuario');
+const passport = require('passport');
 
 //rutas
 router.get('/', (req, res) => {
@@ -10,6 +12,11 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
     res.render('login.html')
 })
+
+router.post('login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+}));
 
 router.get('/register', (req, res) => {
     res.render('register.html')
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 dotenv.config();
-require('./database.js')
+require('./database.js');
+require('../src/models/usuario.js')
 
 //setings
 const PORT = process.env.PORT || 3000;
